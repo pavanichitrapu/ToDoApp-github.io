245bbb2efa319e6b6b4e56fed99dc22067a8c1c3
pavanichitrapu <63398525+pavanichitrapu@users.noreply.github.com>
2020-06-26 17:17:45 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,8 +1,8 @@
 var login=document.getElementById('loginbtn');
 var signup=document.getElementById('signupbtn');
 signup.addEventListener('click',function(){
-    window.location.assign("/login.html");
+    window.location.assign("./login.html");
 });
 login.addEventListener('click',function(){
-    window.location.assign("/login.html");
+    window.location.assign("./login.html");
 });
